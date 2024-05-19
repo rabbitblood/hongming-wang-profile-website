d4cd99d0e4553806384212cfa0180206d94c4b9d
rabbitblood <qz154772610@gmail.com>
2024-05-19 16:51:02 -0700

    update 3d model element

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,26 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  webpack: {
-    rules: [
-      {
-        test: /\.ply$/,
-        use: [{ loader: "ref-loader" }],
-      },
-    ],
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.module.rules.push({
+      test: /\.ply$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next/static/files",
+            outputPath: "static/files",
+            name: "[name].[hash].[ext]",
+          },
+        },
+      ],
+    });
+    return config;
   },
+  transpilePackages: ["three"],
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,14 @@ import My3dModel from "../component/My3dModel/My3dModel";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <My3dModel></My3dModel>
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <My3dModel></My3dModel>
+      </div>
     </main>
   );
 }
