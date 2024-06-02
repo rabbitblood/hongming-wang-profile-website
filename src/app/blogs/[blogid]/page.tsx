bb1bbd71bded2ce6fd7b9550c9ae16dcfe8f86b1
rabbitblood <qz154772610@gmail.com>
2024-06-02 09:29:13 -0700

    init blog page

--- src/app/blogs/[blogid]/page.tsx
+++ src/app/blogs/[blogid]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "@/components/builder";
+import HackerStyleContainer from "@/components/HackerUIComponents/atoms/HackerStyleContainer/HackerStyleContainer";
 
 interface PageProps {
   params: {
@@ -20,9 +21,9 @@ export default async function Page(props: PageProps) {
   console.log(content);
 
   return (
-    <>
+    <HackerStyleContainer>
       {/* Render the Builder page */}
       <RenderBuilderContent content={content} model={"blog-page"} />
-    </>
+    </HackerStyleContainer>
   );
 }
