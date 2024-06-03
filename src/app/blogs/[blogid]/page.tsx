45eaa1c5787e9ab88cc6f68d7cf8f63b43d68191
rabbitblood <qz154772610@gmail.com>
2024-06-03 10:17:52 -0700

    improve blog page seo

--- src/app/blogs/[blogid]/page.tsx
+++ src/app/blogs/[blogid]/page.tsx
@@ -3,9 +3,36 @@ import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "@/components/builder";
 import HackerStyleContainer from "@/components/HackerUIComponents/atoms/HackerStyleContainer/HackerStyleContainer";
 import HackerStyleHeading from "@/components/HackerUIComponents/atoms/HackerStyleHeading/HackerStyleHeading";
-import HackerCodeContainer from "@/components/HackerUIComponents/atoms/HackerCodeContainer/HackerCodeContainer";
 import "./BlogId.css";
+import type { Metadata, ResolvingMetadata } from "next";
 
+type Props = {
+  params: {
+    blogid: string;
+    id: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const content = await builder
+    .get("blog-page", {
+      userAttributes: {
+        urlPath: "/blogs/" + (params?.blogid || ""),
+      },
+    })
+    .toPromise();
+
+  return {
+    title: `${content.data.title} | ${new Date(
+      content.data.blogDate
+    ).toLocaleDateString()} | Hongming Wang - Game/Web Dev`,
+    description: content.data.shortDesc,
+  };
+}
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 interface PageProps {
@@ -23,7 +50,6 @@ export default async function Page(props: PageProps) {
     })
     .toPromise();
 
-  console.log(content.data);
   return (
     <HackerStyleContainer>
       {/* Render the Builder page */}
