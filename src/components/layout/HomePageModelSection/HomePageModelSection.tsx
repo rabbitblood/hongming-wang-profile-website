56f43877de0c6ed8812654275ac9d32f8b9e2d33
rabbitblood <qz154772610@gmail.com>
2024-06-05 02:51:12 -0700

    make 3d model import dynamic

--- src/components/layout/HomePageModelSection/HomePageModelSection.tsx
+++ src/components/layout/HomePageModelSection/HomePageModelSection.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import "./HomePageModelSection.css";
-import My3dModel from "@/components/atoms/My3dModel/My3dModel";
 // three.js and react-three-fiber are not supported in server-side rendering
+
+const My3dModel = dynamic(
+  () => import("@/components/atoms/My3dModel/My3dModel"),
+  {
+    ssr: false,
+  }
+);
 
 export default function HomePageModelSection() {
   return (
